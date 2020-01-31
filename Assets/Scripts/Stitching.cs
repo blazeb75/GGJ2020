16181e18cc3b123efc4cead20ffca4e28d84fec0
Zac Paternoster <blazeb75@gmail.com>
2020-02-01 09:20:28 +1000

    Editor code for stitching

--- Assets/Scripts/Stitching.cs
+++ Assets/Scripts/Stitching.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class Stitching : MonoBehaviour
 {
-
+    public GameObject[] targets;
     public GameObject targetDecal;
+    public Vector3[] points;
+
+    public int currentTarget = 0;
+
+    //private Mesh[] targetMeshes;
+    //private Collider[] targetColliders;
 
     // Start is called before the first frame update
     void Start()
     {
+        targetDecal = Instantiate(targetDecal);
+        //targetMeshes = new Mesh[targets.Length];
+        //targetColliders = new Collider[targets.Length];
+        ////for(int i = 0; i < targets.Length; i++)
+        //{
+        //    targetMeshes[i] = targets[i].GetComponent<MeshFilter>().mesh;
+        //    targetColliders[i] = targets[i].GetComponent<Collider>();
+        //}
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void PlaceTargetDecal()
     {
-        
+        targetDecal.transform.position = points[currentTarget];
     }
-
-    void 
 }
