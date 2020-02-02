0ee4cb01390727702269b01aa9df06f23a91780b
Helayna <helayna.strohfeldt@connect.qut.edu.au>
2020-02-02 16:05:34 +1000

    Pretty Much Done For Today

---
+++ Assets/Scripts/SceneCtrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCtrl : MonoBehaviour
+{
+ 
+    public void ReloadGame()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void QuitGame()
+    {
+        if (!Application.isEditor) System.Diagnostics.Process.GetCurrentProcess().Kill();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#endif
+    }
+}
